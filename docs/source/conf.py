9e28b1d85332c21e6152fac46a9e5ebc3b76d057
selkamand <sam.elkamand@gmail.com>
2023-12-05 11:44:18 +1100

    fix: added CRUX readme and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CRUX'
+copyright = '2023, Childrens Cancer Institute'
+author = 'Sam El-Kamand'
 
 release = '0.1'
 version = '0.1.0'
